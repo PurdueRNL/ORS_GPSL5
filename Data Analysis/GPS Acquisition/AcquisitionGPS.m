1ec00f13a01d0a91b5920b3e374af6e50e7d3cfa
PurdueRNL <hheim@purdue.edu>
2016-05-11 14:30:14 -0400

    Update of GPS Aquisition MATLAB file

--- Data Analysis/GPS Acquisition/AcquisitionGPS.m
+++ Data Analysis/GPS Acquisition/AcquisitionGPS.m
@@ -9,22 +9,21 @@ fI=0000000;% intermediate frequency
 fD=0;
 Ti=0.001;% integration time
 sample_num=floor(fs*Ti); 
-% pfile=fopen('C:\Users\rnllab\Desktop\NRL_Testing\Test_5_GPS_RL\GPSRL_20160216T182352Z_1');
+pfile=fopen('C:\Users\rnllab\Desktop\NRL_Testing\Test_5_GPS_RL\GPSRL_20160216T182352Z_1');
 xR=[];
 xR2=[];
-% for i=1:sample_num
-% xR_temp=fread(pfile,2,'int16'); % Read I and Q components of one sample
-% xR=[xR xR_temp(1)]; % I components of all samples 
-% xR2=[xR2 xR_temp(2)];% Q components of all samples
-% end
+for i=1:sample_num
+xR_temp=fread(pfile,2,'int16'); % Read I and Q components of one sample
+xR=[xR xR_temp(1)]; % I components of all samples 
+xR2=[xR2 xR_temp(2)];% Q components of all samples
+end
 % load('C:\Users\rnllab\Downloads\matlab4\matlab1')
-load('../Test/Data')
 % xR(1:sample_num)=data_I(sample_num*40.5+1:sample_num*41.5);
 % xR2(1:sample_num)=data_Q(sample_num*40.5+1:sample_num*41.5);
 % data_I=real(data_complex);
 % data_Q=imag(data_complex);
-xR(1:sample_num)=data_I(sample_num*40.5+1:sample_num*41.5);
-xR2(1:sample_num)=data_Q(sample_num*40.5+1:sample_num*41.5);
+% xR(1:sample_num)=data_I(sample_num*40.5+1:sample_num*41.5);
+% xR2(1:sample_num)=data_Q(sample_num*40.5+1:sample_num*41.5);
 
 % psd(data_complex);
 for PRN=1:32
@@ -99,111 +98,6 @@ xlim([0 Ti*1023000]);
 ylim([minFreq maxFreq]);
 end
 
-%Galileo
-
-% fs=5000000;
-% fI=000000;
-% fD=0;
-% sample_num=floor(fs*0.004);
-% pfile=fopen('GPSLHCP_20150414183130_B.dat');
-% xR=[];
-% xR2=[];
-% for i=1:sample_num
-% xR_temp=fread(pfile,2,'int16');
-% xR=[xR xR_temp(1)];
-% xR2=[xR2 xR_temp(2)];
-% end
-% cfile=fopen('code.txt');
-% 
-% for PRN=[11 12 14 18 19 20]
-% fseek(cfile, 0, -1);
-% fseek(cfile,1023*(PRN-1), -1);
-% PrimCode=[];
-% for i=1:1023
-% charCode=fscanf(cfile,'%c',1);
-% if(charCode>='A')
-%     charCode=charCode-'A'+10;
-% else
-%     charCode=str2num(charCode);
-% end
-% c1=floor(charCode/8);
-% c2=floor((charCode-c1*8)/4);
-% c3=floor((charCode-c1*8-c2*4)/2);
-% c4=charCode-c1*8-c2*4-c3*2;
-% PrimCode=[PrimCode c1 c2 c3 c4];
-% end
-% PrimCode=PrimCode*(-2)+1;
-% disp(PrimCode);
-% a=(10/11)^0.5;
-% b=(1/11)^0.5;
-% SubE1B=[a+b a-b a+b a-b a+b a-b -a+b -a-b -a+b -a-b -a+b -a-b ];
-% SubE1C=[a-b a+b a-b a+b a-b a+b -a-b -a+b -a-b -a+b -a-b -a+b];
-% Secondary_E1C=[0 0 1 1 1 0 0 0 0 0 0 0 1 0 1 0 1 1 0 1 1 0 0 1 0];
-% 
-% for i=1:sample_num
-%     
-% n=ceil(i*1023000/fs);
-% n=n-4092*floor(n/4092);
-% if (n==0)
-%     n=4092;
-% end
-% Sample_PrimCode(i)=PrimCode(n);
-% 
-% n=ceil(i*1023000*12/fs);
-% n=n-12*floor(n/12);
-% if (n==0)
-%     n=12;
-% end
-% Sample_SubE1C(i)=SubE1C(n);
-% Sample_SubE1B(i)=SubE1B(n);
-% 
-% n=ceil(i*250/fs);
-% n=n-25*floor(n/25);
-% if (n==0)
-%     n=25;
-% end
-% Sample_SecondE1C(i)=Secondary_E1C(25);
-% 
-% end
-% Sample=Sample_PrimCode.*Sample_SubE1C;%.*Sample_SecondE1C;
-% 
-% step=333;
-% minFreq=-6000;
-% maxFreq=6000;
-% FreqBinNum=ceil((maxFreq-minFreq)/333);
-% for i=1:FreqBinNum
-%     
-% fD(i)=minFreq+333*i;
-%     
-% for j=1:sample_num
-% IL(j)=2^0.5*cos(2*pi*(fI+fD(i))*j/fs)*Sample(j);
-% IQ(j)=2^0.5*sin(2*pi*(fI+fD(i))*j/fs)*Sample(j);
-% end
-%     
-% [RI_1, lag]= circcorr(xR,IL,1023000/fs);
-% [RI_2, lag]= circcorr(xR2,IQ,1023000/fs);
-% [RQ_1, lag]= circcorr(xR,IQ,1023000/fs);
-% [RQ_2, lag]= circcorr(xR2,IL,1023000/fs);
-% % 
-% 
-% Z=(RI_1+RI_2).^2+(-RQ_1+RQ_2).^2;
-% % Z=RI_1.^2+RI_2.^2;
-% R(i,1:sample_num)=Z;
-% % [max(i),index(i)]=max(Z);
-% 
-% end
-% 
-% CN0=0;
-%     
-% figure
-% mesh(R);
-% xlabel('Delay/chip');
-% ylabel('Doppler/Hz');
-% zlabel('Correlation');
-% t1 = strcat('Correlation for PRN',num2str(PRN));
-% title(t1);
-% 
-% end
 
 
 
