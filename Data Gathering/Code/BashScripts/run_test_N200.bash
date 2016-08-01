319a0b9ce26bbd9c00f41a82f5927c13b220c6bb
PurdueRNL <hheim@purdue.edu>
2016-08-01 13:16:44 -0400

    These changes added the ability to define headers, subheaders, and subfooters inside the .cpp file. Made quite some time ago, they were only recently committed. Headers occur once per file, at the beginning, and include various information about the collection. Subheaders occur 10 times per file, once per section, and include the time offset from the time included in the header. Subfooters occur 10 times per file, once per section, and include information on drops in that section.

--- Data Gathering/Code/BashScripts/run_test_N200.bash
+++ Data Gathering/Code/BashScripts/run_test_N200.bash
@@ -4,11 +4,12 @@
 freqIn=1.57542 # GHz
 rateIn=25 # MHz
 gain=40 # dB
-rtime=1 # sec
+rtime=1 # Length (in seconds) of one section
 addr="addr=192.168.10.2" # IP address of N200
 dtype="sc8" # Data type
-to_collect=14400 # Number of collections in a batch
-nfiles=14400 # Total number of datafiles to collect
+section_amount=10 # Number of sections in each file
+to_collect=6 # Number of files to collect before reconnecting
+nfiles=14400 # Total number of datafiles to collect (-1 for infinite)
 
 sudo sysctl -w net.core.rmem_max=480000000
 sudo sysctl -w net.core.wmem_max=480000000
@@ -28,7 +29,7 @@ while (($run <= ($nfiles / $to_collect))); do
     tstamp=$(date "+%Y%m%d%H%M%S")
 
     # Set filenames
-    dfile="../../Data/XMTest_${tstamp}.dat" # Datafile
+    dfile="../../Data/XMTest_" # Datafile
     rfile="../../Data/XMTest_${tstamp}.txt" # Log file
 
     # Calculate batch number
@@ -40,7 +41,7 @@ while (($run <= ($nfiles / $to_collect))); do
     echo "Creating files starting at $dfile"
 
     # Run binary program, passing arguments through command line, to collect data
-    ../CppProgram/Other/rx_multi_samplesv3 --args "$addr" --time $rtime --to_collect $to_collect --rate $rate --subdev "$subdev" --freq $freq --file "$dfile" --gain "$gain" --wirefmt "$dtype" --cpufmt "$dtype"
+    ../CppProgram/Other/rx_multi_samplesv3 --args "$addr" --time $rtime --to_collect $to_collect --section_amount $section_amount --rate $rate --subdev "$subdev" --freq $freq --file "$dfile" --gain "$gain" --wirefmt "$dtype" --cpufmt "$dtype"
 
     # Increment loop counter variable
     let run=$run+1
